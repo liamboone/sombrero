b2b43c0c8ec55adcf5010664445e02f8c34f4e23
Liam <liamboone@gmail.com>
2013-03-11 00:04:15 -0400

    herokuapp test

--- sombreroApp.py
+++ sombreroApp.py
@@ -117,4 +117,5 @@ def index():
     return render_template('sombrero.html')
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0', port=port)
